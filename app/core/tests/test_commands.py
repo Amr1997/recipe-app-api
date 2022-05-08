88a9b4797f5dd4468d8e92727af264ae6739bedd
amr97 <amr_maher1@outlook.com>
2022-05-08 19:49:50 +0200

    final changes

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -5,16 +5,15 @@ from django.db.utils import OperationalError
 from django.test import TestCase
 
 
-
 class CommandTests(TestCase):
-    
+
     def test_wait_for_db_ready(self):
         """Test waiting for db when db is available"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.return_value = True
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 1)
-    
+
     @patch('time.sleep', return_value=True)
     def test_wait_for_db(self, ts):
         """Test waiting for db"""
@@ -22,4 +21,3 @@ class CommandTests(TestCase):
             gi.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 6)
-            
